e77cfdeee0db8103b50ad0495a62558aa49cebc9
0xMautin <alayoquadrimautin1514@gmail.com>
2025-03-14 10:48:19 +0100

    Added grading app

---
+++ app.py
@@ -0,0 +1,10 @@
+score = int(input("Enter your score: "))
+
+if score >= 80:
+    print("Excellent")  
+elif score >= 60:
+    print("Very good")
+elif score >= 40:
+    print("Good")
+elif score >=0:
+    print("You need to study more")
